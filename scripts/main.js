9acefc3c43f95ddbd48188a0e13271b649875851
txff99 <txfff99@gmail.com>
2023-09-01 22:37:18 +0800

    test2

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,50 @@
     
     // });
 // const fs = require('fs');
-// define(['require','fs'], function (require) {
+// def`ine(['require','fs'], function (require) {
 // });
 // require(['fs'],function(fs){});
-var LINK_SRC = "https://github.com/txff99/txff99.github.io/link/link.txt";
-var DES_SRC = "https://github.com/txff99/txff99.github.io/link/description.txt"
+// https://raw.githubusercontent.com/txff99/txff99.github.io/main/link/link.txt
+// https://api.github.com/repos/txff99/txff99.github.io/contents/link/link.txt
+var LINK_SRC = "https://api.github.com/repos/txff99/txff99.github.io/contents/link/link.txt";
+var DES_SRC = "https://github.com/txff99/txff99.github.io/blob/main/link/de`scription.txt"
 const links = [];
 const description = [];
 
-fetch(LINK_SRC)
-  .then((res) => res.text())
-  .then((text) => {
-    console.log(text);
-    // links.push(text)
-   })
-  .catch((e) => console.error(e));
+// let headers = new Headers();
+
+// headers['Access-Control-Allow-Origin'] = '*'
+// headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+// headers['Access-Control-Request-Method'] = '*'
+// headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
+
+// // headers.append('Content-Type', 'application/text');
+// // headers.append('Accept', 'application/text');
+// // headers.append('Authorization', 'Basic ' );
+// // headers.append('Origin','http://localhost:3000');
+
+const getSampleText = async()=> {
+    await fetch(LINK_SRC,{
+        // mode: 'cors',
+        // credentials: 'include',
+        // method: 'POST',
+        // headers: headers
+    })
+      .then((res) => res.json())
+      .then((text) => {
+        console.log(text["content"]);
+        // links.push(text)
+       })
+      .catch((e) => console.error(e));
+  };
+getSampleText();
+
+// function reqListener () {
+//     console.log(this.responseText);
+//   }
+  
+//   var oReq = new XMLHttpRequest();
+//   oReq.addEventListener("load", reqListener);
+//   oReq.open("GET", LINK_SRC);
+//   oReq.send();
