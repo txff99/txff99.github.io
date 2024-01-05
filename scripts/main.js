4be6708658726cd5203365292dd825d87388018f
txff99 <txfff99@gmail.com>
2024-01-05 12:38:02 -0800

    add lru

--- scripts/main.js
+++ scripts/main.js
@@ -12,53 +12,88 @@ var DES_SRC = "https://api.github.com/repos/txff99/txff99.github.io/contents/lin
 
 const links = [];
 const descriptions = [];
-var num = -1;
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
 
-const Change_Image_Random = async()=> {
-    num = -1
-    await fetch(LINK_SRC,{
-    })
-    .then((res) => res.json())
-    .then((text) => {
-        const tmp_link = atob(text["content"]).split('\n');
-        if(num==-1){
-            num = getRandomInt(tmp_link.length);
-            
-        }
-        console.log(tmp_link[num]);
-        document.getElementById("frame").src=tmp_link[num];
-        
-    })
-    .catch((e) => console.error(e));
-        
-    await fetch(DES_SRC,{
-    })
-    .then((res) => res.json())
-    .then((text) => {
-        const tmp_des = atob(text["content"]).split('\n');
-        if(num==-1){
-            num = getRandomInt(tmp_link.length);
-        }
-        document.getElementById("video_description").innerHTML=tmp_des[num];
-        
-
-    })
-    .catch((e) => console.error(e));
-    // return [links,descriptions];
+function createRange(start, end, step = 1) {
+    let range = [];
     
-    // for(var i=0;i<=links.length;++i){
-    //     console.log(links[i]);
-    // }
-};
-
-// const getdes = async()=> {
-    // };
-Change_Image_Random();
+    for (let i = start; i < end; i += step) {
+        range.push(i);
+    }
     
+    return range;
+}
+
+class Wrapper {
+    constructor() {
+        this.video_links = [];
+        this.video_descriptions = [];
+    }
+
+    async Init() {
+        await this.GetVideoLink();
+        await this.GetVideoDescription();
+    }
+
+    TrackTransaction(n) {
+        //to avoid duplication
+        const index = this.element_list.indexOf(n);
+        this.element_list.splice(index, 1);
+        if(this.element_list.length==0){
+            this.element_list = createRange(0,this.content_num);
+          }
+    }
+    
+    GetRandomInt(){
+        return this.element_list[Math.floor(Math.random()*this.element_list.length)];
+    }
+    
+    PostOnHTML(){
+        var num = this.GetRandomInt();
+        this.TrackTransaction(num);
+        console.log(this.element_list.length);
+        document.getElementById("frame").src=this.video_links[num];
+        document.getElementById("video_description").innerHTML=this.video_descriptions[num];
+    }
+    
+    GetVideoLink(){
+        return new Promise((resolve)=>{
+            fetch(LINK_SRC,{
+            })
+            .then((res) => res.json())
+            .then((text) => {
+                this.video_links = atob(text["content"]).split('\n');
+                this.content_num = this.video_links.length;
+                this.element_list = createRange(0,this.content_num);
+                resolve();
+            })
+            .catch((e) => console.error(e));
+        });
+    }
+
+    GetVideoDescription(){
+        return new Promise((resolve)=>{
+            fetch(DES_SRC,{
+            })
+            .then((res) => res.json())
+            .then((text) => {
+                this.video_descriptions = atob(text["content"]).split('\n');
+                resolve();
+            })
+            .catch((e) => console.error(e));
+        });
+    }
+
+
+  };
+
+async function InitializeWrapper(wrapper){
+    await wrapper.Init();
+    wrapper.PostOnHTML();
+}
+
+var wrapper = new Wrapper();
+InitializeWrapper(wrapper);
+
 
 
